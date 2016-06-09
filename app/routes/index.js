cfc35719e13377cb23a630382e55862ef40e2434
Niem Nguyen and Pablo Vera <niem@>
2016-06-09 16:12:03 -0700

    can update rentals

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,9 +5,18 @@ export default Ember.Route.extend({
     return this.store.findAll('rental');
   },
   actions: {
-    save3(params) {
-      var newRental = this.store.createRecord('rental', params);
-      newRental.save();
+    save3(paramsqqq) {
+      var newRentalsss = this.store.createRecord('rental', paramsqqq);
+      newRentalsss.save();
+      this.transitionTo('index');
+    },
+    update(rental, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          rental.set(key,params[key]);
+        }
+      });
+      rental.save();
       this.transitionTo('index');
     },
     destroyRental(anything) {
