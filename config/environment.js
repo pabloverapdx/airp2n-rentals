641f245bd584154fa854cffc449b188e90d61852
Niem Nguyen and Pablo Vera <niem@>
2016-06-09 09:02:07 -0700

    add tutorial.md

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'airp2n',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://airp2n.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
